541ea62ba6834be19dbdb8293da49eab6519845c
hartonamalia <hartonamalia99@yahoo.com>
2024-04-15 22:01:27 +0300

    navigate home

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Navlinks = [
   {
@@ -24,27 +25,36 @@ export const Navlinks = [
     name: "PROFILE",
     link: "/profile",
   },
-  
 ];
 const Navbar = ({ theme, setTheme }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <div
-      className="relative z-10 shadow-md w-full duration-300 bg-violet-500">
+    <div className="relative z-10 shadow-md w-full duration-300 bg-violet-500">
       <div className="container py-2 md:py-0">
         <div className="flex justify-between items-center">
           <div>
-            <span className="text-3xl font-bold cursor-pointer">TransShare</span>
+            <span
+              className="text-3xl font-bold cursor-pointer"
+              onClick={handleClick}
+            >
+              TransShare
+            </span>
           </div>
           <nav className="hidden md:block">
             <ul className="flex items-center gap-8">
-              {Navlinks.map(({ id, name, link,index }) => (
+              {Navlinks.map(({ id, name, link, index }) => (
                 <li key={id} className="py-4">
-                  <Link to={link}
+                  <Link
+                    to={link}
                     className=" text-lg font-medium  hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500  "
                   >
                     {name}
